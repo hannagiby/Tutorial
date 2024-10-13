4ed9c0533f37971335d112e0517de637c4003366
hannagiby <hannamariagiby2027@cs.sjcetpalai.ac.in>
2024-10-13 14:42:18 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,21 @@
+package javalessons;
+import java.util.Scanner;
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the string:");
+		String str= sc.nextLine();
+		int len=str.length();
+		String rev="";
+		for(int i=len-1;i>=0;i--)
+		{
+			rev=rev+str.charAt(i);
+		}
+		System.out.println("The reversed string is:"+rev);
+		
+
+	}
+
+}
